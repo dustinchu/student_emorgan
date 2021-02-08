0928dd6dbfb3abcc2f805f9ad9726717d2e3bd83
Dustin <dustin_chu@hotmail.com>
2021-02-08 18:22:31 +0800

    test

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "a00ab08752a036b64a8f26683cc3a002",
-"/": "a00ab08752a036b64a8f26683cc3a002",
-"main.dart.js": "c4ef904753eaf5b90eaf9721e462a3b9",
+"index.html": "21960c7b3895567073443ba23873deaf",
+"/": "21960c7b3895567073443ba23873deaf",
+"main.dart.js": "a55f3f50b492d4d77fb2326d22f08e3c",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
