2f1cb7d8539f957e9bf8d968914b4235e1902e11
Dustin <dustin_chu@hotmail.com>
2021-02-24 17:23:36 +0800

    add vidio

--- lib/screen/home/home_vidio.dart
+++ lib/screen/home/home_vidio.dart
@@ -1,4 +1,7 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeVidio extends StatefulWidget {
   HomeVidio({Key key}) : super(key: key);
@@ -7,17 +10,83 @@ class HomeVidio extends StatefulWidget {
   _HomeVidioState createState() => _HomeVidioState();
 }
 
+// https://vod-progressive.akamaized.net/exp=1614157174~acl=%2A%2F617711126.mp4%2A~hmac=f43820930d5cc210055084405c3684d7a2d3e21a48c8ed870ae4f837e1584121/vimeo-prod-skyfire-std-us/01/2418/7/187094433/617711126.mp4
 class _HomeVidioState extends State<HomeVidio> {
+  TargetPlatform _platform = TargetPlatform.android;
+  VideoPlayerController _videoPlayerController1;
+  VideoPlayerController _videoPlayerController2;
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController1 = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
+    await _videoPlayerController1.initialize();
+    _videoPlayerController2 = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4');
+    await _videoPlayerController2.initialize();
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
+      autoPlay: true,
+      looping: true,
+      // Try playing around with some of these other options:
+
+      showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
-    // double h = MediaQuery.of(context).size.height;
-    double h = 1400;
+    double h = 1500;
     return Container(
       width: w,
       height: h,
+      decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/big_banner_backroung.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
       child: Center(
-        child: Text("111"),
+        child: _chewieController != null &&
+                _chewieController.videoPlayerController.value.initialized
+            ? Chewie(
+                controller: _chewieController,
+              )
+            // : Container()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // CircularProgressIndicator(),
+                  // SizedBox(height: 20),
+                  Text('Loading'),
+                ],
+              ),
       ),
     );
   }
