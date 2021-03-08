6cf3f36af4473635bfc39ab0e0f75476555a42a9
Dustin <dustin_chu@hotmail.com>
2021-03-08 22:59:54 +0800

    商品頁 shial page2 高度355

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "659c53282b4a41ffd5c4759aa0bf77ea",
-"/": "659c53282b4a41ffd5c4759aa0bf77ea",
-"main.dart.js": "47021a9734ebb4a5ff374a1b7deecbb4",
+"index.html": "3e300462b57cb59c5e07f3b6dea46bfc",
+"/": "3e300462b57cb59c5e07f3b6dea46bfc",
+"main.dart.js": "183ae2b256a9d8b12c0efa2cae23be0b",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
