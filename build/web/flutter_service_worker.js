a316c761da10245d8aa8be3431674d0df9355c45
Dustin <dustin_chu@hotmail.com>
2021-03-08 22:49:33 +0800

    商品頁 shail  page2 高度342

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "4c3b522ee293946f69cc2503a93b5fc4",
-"/": "4c3b522ee293946f69cc2503a93b5fc4",
-"main.dart.js": "c2f2c87dabd3ef14602b6a36028ef8c8",
+"index.html": "d7ca4c1b1ad1fc3b26d2690a1065dcd4",
+"/": "d7ca4c1b1ad1fc3b26d2690a1065dcd4",
+"main.dart.js": "3a53512a82149e75a65f0b6854140db5",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
