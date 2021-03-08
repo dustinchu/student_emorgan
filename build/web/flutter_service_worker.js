a36cfbe2342b0b4e05e9e6f13847ad7f56536c78
Dustin <dustin_chu@hotmail.com>
2021-03-08 22:25:29 +0800

    修改 shila page2 長寬

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "43dfd86fdbbb64f71ad78d7c8c9324c4",
-"/": "43dfd86fdbbb64f71ad78d7c8c9324c4",
-"main.dart.js": "b927afc3b0dc7b2850cadf28ee9c6a67",
+"index.html": "b24f68ee7d5448c60324cb8de8523a45",
+"/": "b24f68ee7d5448c60324cb8de8523a45",
+"main.dart.js": "7324149863536244ccdbd911376e1201",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
