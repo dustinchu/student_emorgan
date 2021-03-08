bcea6d81fd708fde82aaed3cca2077feee6a6331
Dustin <dustin_chu@hotmail.com>
2021-03-08 22:07:20 +0800

    測試商品頁page5 高度 build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "3844375025fcbf80c50dc1d2f40425c5",
-"/": "3844375025fcbf80c50dc1d2f40425c5",
-"main.dart.js": "42f09f651639fa615e491252e64edfac",
+"index.html": "27f20574fb7ed9198c62f90e348f8c23",
+"/": "27f20574fb7ed9198c62f90e348f8c23",
+"main.dart.js": "e337493f53a398aa82a6af230a188c5d",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
