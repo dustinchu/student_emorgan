3367cc5cf1b6169c82ab2e38cb6ce30614fb3674
Dustin <dustin_chu@hotmail.com>
2021-03-08 22:54:35 +0800

    商品頁 shila page2 高度 352

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d437f1cbf3f5ac9b7a710fa814217dbf",
-"index.html": "d7ca4c1b1ad1fc3b26d2690a1065dcd4",
-"/": "d7ca4c1b1ad1fc3b26d2690a1065dcd4",
-"main.dart.js": "3a53512a82149e75a65f0b6854140db5",
+"index.html": "659c53282b4a41ffd5c4759aa0bf77ea",
+"/": "659c53282b4a41ffd5c4759aa0bf77ea",
+"main.dart.js": "47021a9734ebb4a5ff374a1b7deecbb4",
 "favicon.png": "bded570080bd67f70737d5b23cad022f",
 "icons/Icon-192.png": "42c2aa62a4a11c93b56bb73a70d570a3",
 "icons/Icon-512.png": "5986ba0e4e756ec1c4ddd53e245b03d1",
